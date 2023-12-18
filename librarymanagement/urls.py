1020f8fadb999feeb58d5426833d1919e13701e5
vasusingh52 <153858710+vasusingh52@users.noreply.github.com>
2023-12-18 19:29:18 +0000

    Update urls.py

--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
 
     path('adminsignup', views.adminsignup_view),
     path('studentsignup', views.studentsignup_view),
-    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html')),
-    path('studentlogin', LoginView.as_view(template_name='library/studentlogin.html')),
+    path('adminlogin', CustomAdminLoginView.as_view(), name='adminlogin'),
+    path('studentlogin', CustomStudentLoginView.as_view(), name='studentlogin'),
 
     path('logout', LogoutView.as_view(template_name='library/index.html')),
     path('afterlogin', views.afterlogin_view),
