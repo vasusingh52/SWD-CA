94cdfdf97d94fad69d302aa2c3314f1fd126f45f
vasusingh52 <153858710+vasusingh52@users.noreply.github.com>
2023-12-19 11:46:26 +0000

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'axes.middleware.AxesMiddleware’
+    'django.axes.middleware.AxesMiddleware’
       
 ]
 
