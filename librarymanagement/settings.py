c5365b8bb44cf5b90ce88068a1f6d4159ddce923
vasusingh52 <153858710+vasusingh52@users.noreply.github.com>
2023-12-21 21:56:58 +0000

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY=config{'SECRET_KEY'}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
